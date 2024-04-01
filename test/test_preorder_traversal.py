6217dfcd0e136ab7dd8fec6d05d33805b8636a5c
ihor1403 <10igorik03@gmail.com>
2024-04-01 11:05:59 +0300

    add/lab3

---
+++ test/test_preorder_traversal.py
@@ -0,0 +1,21 @@
+import unittest
+from lab3 import BinaryTree
+from lab3 import pre_order_traversal
+
+class TestPreOrderTraversal(unittest.TestCase):
+    def test_pre_order_traversal(self):
+        root = BinaryTree(1)
+        root.left = BinaryTree(2)
+        root.right = BinaryTree(3)
+        root.left.left = BinaryTree(4)
+        root.left.right = BinaryTree(5)
+
+        expected_result = [1, 2, 4, 5, 3]
+
+        result = pre_order_traversal(root)
+
+
+        self.assertEqual(result, expected_result)
+
+if __name__ == '__main__':
+    unittest.main()
